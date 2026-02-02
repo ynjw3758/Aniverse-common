6ca8c0bc74f7a213535f94e0a25d18bc01d12876
ynjw3758 <yoonjw7894@naver.com>
2026-02-02 17:32:05 +0900

    애완동물 등록 서비스 api 작업 중

---
+++ src/main/java/com/Aniverse/Common/mapper/Pets/Pets_Mapper.java
@@ -0,0 +1,10 @@
+package com.Aniverse.Common.mapper.Pets;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+@Mapper
+@Repository
+public interface Pets_Mapper {
+
+}
